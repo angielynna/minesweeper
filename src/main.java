c64ffb4aad77809b8b21b46b0af5b3749564cc2d
Angelynna Pyeatt <angelynnapyeatt@Angelynnas-MacBook-Air.local>
2022-10-04 23:55:35 -0700

    Update mineFieldGenerator

--- src/main.java
+++ src/main.java
@@ -4,26 +4,26 @@ import java.util.*;
 public class main {
     public static void main(String[] theArgs) throws
             FileNotFoundException {
-        File output = new File("output.txt");
 
+        File output = new File("output.txt");
         Scanner sc = null;
         StringBuilder sb = new StringBuilder();
-
         try{
             sc = new Scanner(new File(theArgs[0]));
             while(sc.hasNext()) {
                 int rows = sc.nextInt();
                 int columns = sc.nextInt();
-                char[][] mineField = new char[rows][columns];
+                String[][] mineField = new String[rows][columns];
                 for(int i = 0; i < rows; i++) {
                     for(int j = 0; j < columns; j++) {
-
+                        mineField[i][j] = sc.next();
                     }
                 }
             }
         } catch(FileNotFoundException e) {
             throw new FileNotFoundException("File not found " + e);
         }
+
     }
 
 }
--- src/mineFieldGenerator.java
+++ src/mineFieldGenerator.java
@@ -15,6 +15,7 @@ public class mineFieldGenerator {
         if ((int) (bombSpawnRate * (rows * columns)) > 1) {
             numBombs = (int) (bombSpawnRate * (rows * columns));
         }
+        System.out.println(numBombs);
         generateLocations(rows, columns, numBombs);
     }
 
@@ -22,32 +23,59 @@ public class mineFieldGenerator {
                                           int theBombs) throws
                                         FileNotFoundException{
         Random rand = new Random();
-        StringBuilder field = new StringBuilder();
-        int bombRow = rand.nextInt(theRows + 1) - 1;
-        int bombCol = rand.nextInt(theCols + 1) - 1;
-        while (theBombs > 0) {
-            for (int i = 0; i < theRows; i++) {
-                for (int j = 0; j < theCols; j++) {
-                    if (i == bombRow && j == bombCol) {
-                        field.append('*');
-                        bombRow = rand.nextInt(theRows);
-                        bombCol = rand.nextInt(theCols);
-                    } else {
-                        field.append('.');
-                    }
-                }
-                field.append("\n");
+        char[][] field = new char[theRows][theCols];
+        StringBuilder sb = new StringBuilder();
+        sb.append(theRows + " " + theCols + "\n");
+
+        //generate bomb locations
+        int[][] bombs = genBombLocation(theBombs, theRows, theCols);
+
+        //filling field with bombs
+        for(int i = 0; i < bombs.length; i++) {
+            for(int j = 0; j < 2; j++) {
+                field[bombs[i][0]][bombs[i][1]] = '*';
             }
-            theBombs--;
         }
-        output(field);
+
+        //filling the rest of the field:
+        for(int i = 0; i < theRows; i++){
+            for (int j = 0; j < theCols; j++) {
+                if(field[i][j] != '*') {
+                    field[i][j] = '.';
+                }
+            }
+        }
+
+        output(field, sb);
     }
 
-    private static void output(StringBuilder theField)
+    private static int[][] genBombLocation(int theBombs, int theRows,
+                                           int theColumns) {
+        int[][] locations = new int[theBombs][2];
+        Random rd = new Random();
+        for(int i = 0; i < theBombs; i++) {
+            int bombRow = rd.nextInt(theRows);
+            for(int j = 0; j < 1; j++) {
+                int bombCol = rd.nextInt(theColumns);
+                locations[i][j] = bombRow;
+                locations[i][j + 1] = bombCol;
+            }
+        }
+        return locations;
+    }
+
+    private static void output(char[][] theField,
+                               StringBuilder theString)
             throws FileNotFoundException {
         try {
             PrintStream output = new PrintStream("generated.txt");
-            output.append(theField.toString());
+            for(int i = 0; i < theField.length; i++) {
+                for(int j = 0; j < theField[0].length; j++) {
+                    theString.append(theField[i][j]);
+                }
+                theString.append("\n");
+            }
+            output.append(theString.toString());
         } catch (FileNotFoundException e) {
             throw new FileNotFoundException("Cannot find file " + e);
         }
