9a1d9713312fd3f63a7c7f61adbae7e38449c95e
angielynna <angelynnapyeatt@Angelynnas-MacBook-Air.local>
2022-10-11 21:53:57 -0700

    Fixed to read from System.in and print to System.out

--- src/main.java
+++ src/main.java
@@ -1,84 +1,65 @@
-/**
- * TCSS 360
- * Instructor: Tom Capaul
- * Assignment 1 - main.java
- */
-
-import java.io.*;
-import java.util.*;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
 
 /**
- * Displays minesweeper minefield with all adjacency values and bombs
+ * Displays minesweeper minefield with all adjacency values and bombs.
  *
  * @author Angelynna Pyeatt
  * @version October 9, 2022
  */
-public class main {
-    /**
-     * Main method
-     * Takes in a file name from the console as program arguments and displays
-     * the correct minefield
-     *
-     * @param theArgs
-     * @throws Exception
-     */
-    public static void Main(final String[] theArgs) throws FileNotFoundException {
-        Scanner sc = new Scanner(System.in);
+class main {
+    public static void main(final String[] theArgs)
+            throws FileNotFoundException {
+        final Scanner sc = new Scanner(System.in);
         int numFields = 0;
-        if(theArgs.length == 1) {   //reads from theArgs
-            try {
-                //open the file
-                sc = new Scanner(new File(theArgs[0]));
-                int rows = sc.nextInt();
-                int cols = sc.nextInt();
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
 
-                while (rows != 0 || cols != 0) {
-                    numFields++;
-                    //fill minefield:
-                    char[][] mineField = new char[rows + 1][cols + 1];
-                    for (int i = 0; i < rows; i++) {
-                        char[] line = sc.next().toCharArray();
-                        for (int j = 0; j < cols; j++) {
-                            if(j > 0 && i > 0) {
-                                mineField[i][j] = line[j];
-                            }
-                        }
+        while (rows != 0 || cols != 0) {
+            numFields++;
+            //fill minefield:
+            final char[][] mineField = new char[rows][cols];
+            for (int i = 0; i < rows; i++) {
+                final char[] line = sc.next().toCharArray();
+                for (int j = 0; j < cols; j++) {
+                    if (j > 0 && i > 0) {
+                        mineField[i][j] = line[j];
                     }
-                    char[][] generatedNumField = generateNums(mineField);
-
-                    //display correct output
-                    System.out.println("Field #" + numFields + ":");
-                    for(int i = 0; i < generatedNumField.length; i++) {
-                        for (int j = 0; j < generatedNumField[0].length; j++) {
-                            System.out.print(generatedNumField[i][j]);
-                        }
-                        System.out.println();
-                    }
-
-                    rows = sc.nextInt();
-                    cols = sc.nextInt();
                 }
-            } catch (FileNotFoundException e) {
-                throw new FileNotFoundException("File not found " + e);
             }
+            final char[][] generatedNumField = generateNums(mineField);
+
+            //display correct output to System.out
+            final PrintStream console = new PrintStream(System.out);
+            System.setOut(console);
+            console.println("Field #" + numFields + ":");
+            for (int i = 0; i < generatedNumField.length; i++) {
+                for (int j = 0; j < generatedNumField[0].length; j++) {
+                    console.print(generatedNumField[i][j]);
+                }
+                console.println();
+            }
+
+            rows = sc.nextInt();
+            cols = sc.nextInt();
         }
-        sc.close();
 
     }
 
     /**
-     * this method fills a character array with the adjacency values for the
-     * minefield
+     * This method fills a character array with the adjacency values for the
+     * minefield.
      *
      * @param theMineField
      * @return numFields
      */
     private static char[][] generateNums(final char[][] theMineField) {
-        char[][] numFields = new
+        final char[][] numFields = new
                 char[theMineField.length][theMineField[0].length];
-        for(int i = 0; i < theMineField.length; i++) {
-            for(int j = 0; j < theMineField[0].length; j++) {
-                if(theMineField[i][j] == '*') {     //bomb
+        for (int i = 0; i < theMineField.length; i++) {
+            for (int j = 0; j < theMineField[0].length; j++) {
+                if (theMineField[i][j] == '*') {     //bomb
                     numFields[i][j] = '*';
                 } else { //safe space
                     numFields[i][j] = getAdjacencyValue(theMineField, i, j);
@@ -89,8 +70,8 @@ public class main {
     }
 
     /**
-     * method calculates the adjacency values for a given position in
-     * the minefield
+     * Method calculates the adjacency values for a given position in
+     * the minefield.
      *
      * @param theMineField
      * @param theRow
@@ -98,7 +79,8 @@ public class main {
      * @return (char) (value + '0')
      */
     private static char getAdjacencyValue(final char[][] theMineField,
-                                          int theRow, int theCol) {
+                                          final int theRow,
+                                          final int theCol) {
         int value = 0;      //adjacency
 
         //Process of verification:
@@ -106,47 +88,47 @@ public class main {
         //2. If positions directly adjacent are a bomb
 
         //check row above:
-        if(inRange(theRow - 1, theCol - 1, theMineField)) {
-            if(theMineField[theRow - 1][theCol - 1] == '*') {
+        if (inRange(theRow - 1, theCol - 1, theMineField)) {
+            if (theMineField[theRow - 1][theCol - 1] == '*') {
                 value++;
             }
         }
-        if(inRange(theRow - 1, theCol, theMineField)) {
-            if(theMineField[theRow - 1][theCol] == '*') {
+        if (inRange(theRow - 1, theCol, theMineField)) {
+            if (theMineField[theRow - 1][theCol] == '*') {
                 value++;
             }
         }
-        if(inRange(theRow - 1, theCol + 1, theMineField)) {
-            if(theMineField[theRow - 1][theCol + 1] == '*') {
+        if (inRange(theRow - 1, theCol + 1, theMineField)) {
+            if (theMineField[theRow - 1][theCol + 1] == '*') {
                 value++;
             }
         }
 
         //current row:
-        if(inRange(theRow, theCol - 1, theMineField)) {
-            if(theMineField[theRow][theCol - 1] == '*') {
+        if (inRange(theRow, theCol - 1, theMineField)) {
+            if (theMineField[theRow][theCol - 1] == '*') {
                 value++;
             }
         }
-        if(inRange(theRow, theCol + 1, theMineField)) {
-            if(theMineField[theRow][theCol + 1] == '*') {
+        if (inRange(theRow, theCol + 1, theMineField)) {
+            if (theMineField[theRow][theCol + 1] == '*') {
                 value++;
             }
         }
 
         //row below:
-        if(inRange(theRow + 1, theCol - 1, theMineField)) {
-            if(theMineField[theRow + 1][theCol - 1] == '*') {
+        if (inRange(theRow + 1, theCol - 1, theMineField)) {
+            if (theMineField[theRow + 1][theCol - 1] == '*') {
                 value++;
             }
         }
-        if(inRange(theRow + 1, theCol, theMineField)) {
-            if(theMineField[theRow + 1][theCol] == '*') {
+        if (inRange(theRow + 1, theCol, theMineField)) {
+            if (theMineField[theRow + 1][theCol] == '*') {
                 value++;
             }
         }
-        if(inRange(theRow + 1, theCol + 1, theMineField)) {
-            if(theMineField[theRow + 1][theCol + 1] == '*') {
+        if (inRange(theRow + 1, theCol + 1, theMineField)) {
+            if (theMineField[theRow + 1][theCol + 1] == '*') {
                 value++;
             }
         }
@@ -155,17 +137,17 @@ public class main {
 
     /**
      * Determines whether the row and column position set in the parameter
-     * the minefield are legal parameters
+     * the minefield are legal parameters.
      *
      * @param theRow
      * @param theCol
      * @param theMaze
      * @return boolean
      */
-    private static boolean inRange(int theRow, int theCol,
-                                   char[][] theMaze) {
-        if(theRow < theMaze.length && theRow >= 0 &&
-                theCol < theMaze[0].length && theCol >= 0) {
+    private static boolean inRange(final int theRow, final int theCol,
+                                   final char[][] theMaze) {
+        if (theRow < theMaze.length && theRow >= 0
+                && theCol < theMaze[0].length && theCol >= 0) {
             return true;
         }
         return false;
